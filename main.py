ea540ba9c5aa85001d6b8b294e8c3eca80754258
Anirban Ghosh <anirbanghosh060@gmail.com>
2025-05-13 09:51:59 +0530

    remove unused import of types from main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import os
 import tempfile
 from PIL import Image
 import pytesseract
-from google.genai import types
 from dotenv import load_dotenv
 load_dotenv()  # Add this near the top of your file
 
